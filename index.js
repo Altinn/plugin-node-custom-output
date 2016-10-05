31154b1513f3b8fca5d86c690394b993c6169309
David Emanuelsen <david.emanuelsen@outlook.com>
2016-10-05 14:50:29 +0200

    Refactored, updated snippet template to contain comment with version / name, changed the output location of snippets to /exports/snippets

--- index.js
+++ index.js
@@ -1,113 +1,44 @@
-var pluginName = 'plugin-node-custom-output'; var path = require('path');
-var fs = require('fs-extra'); var glob = require('glob');
-var matter = require('gray-matter');
+var pluginName = 'plugin-node-custom-output'; 
+var path = require('path');
+var fs = require('fs-extra'); 
+var glob = require('glob');
+var util = require('./src/util');
+var stringExtensions = require('./src/extensions/stringExtensions');
+var snippetTemplate = require('./src/templates/snippetTemplate');
 
-function fileExists (filePath) {
-  try {
-    return fs.statSync(filePath).isFile()
-  } catch (err) { return false }
-}
 function onPatternIterate (patternlab, pattern) {
-  // TODO: Move to extensions file
-  if (!String.format) {
-    String.format = function(format) {
-      var args = Array.prototype.slice.call(arguments, 1);
-      return format.replace(/{(\d+)}/g, function(match, number) { 
-        return typeof args[number] != 'undefined'
-          ? args[number] 
-          : match
-        ;
-      });
-    };
-  }
-  // END TODO
-
-  // TODO: Move to seperate file
-  var template = "<CodeSnippet Format=\"1.1.0\" xmlns=\"http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet\">"
-    + "\n\t<Header>"
-    + "\n\t\t<Title>{0}</Title>"
-    + "\n\t\t<Author>Altinn</Author>"
-    + "\n\t\t<Shortcut>{1}</Shortcut>"
-    + "\n\t\t<Description>{2}</Description>"
-    + "\n\t\t<SnippetTypes>"
-    + "\n\t\t\t<SnippetType>Expansion</SnippetType>"
-    + "\n\t\t\t<SnippetType>SurroundsWith</SnippetType>"
-    + "\n\t\t</SnippetTypes>"
-    + "\n\t</Header>"
-    + "\n\t<Snippet>"
-    + "\n\t\t<Declarations>"
-    + "\n\t\t</Declarations>"
-    + "\n\t\t<Code Language=\"html\"><![CDATA[{3}]]></Code>"
-    + "\n\t</Snippet>"
-    + "\n</CodeSnippet>";
-  // END TODO
-
   if (pattern.relPath.indexOf('probably-not-needed') === -1 &&
     pattern.relPath.indexOf('.mustache') !== -1) {
-    var patternFile = fs.readFileSync(path.resolve(patternlab.config.paths.public.patterns 
-      + pattern.relPath.replace(/\\/g, '-').replace('.mustache', '/') 
-      + pattern.relPath.replace(/\\/g, '-')), 'utf8');
 
+    // TODO: Get from config (patternlab.config?)
     var currentVersion = 5;
-    var markdownObject = getPatternMarkdownObject(patternlab, pattern);
-    if (markdownObject.data.version == currentVersion) {
-      console.log('Matching version');
+    var markdownObject = util.getPatternMarkdownObject(patternlab, pattern);
+
+    if (markdownObject.data.version == patternlab.config.releaseVersion) {
       updateVersionDependentPatterns(patternlab, pattern, markdownObject.data.version);
     }
 
-    patternFile = String.format(template, "test", "test", "test", patternFile);
-
-    fs.outputFileSync(patternlab.config.paths.public.patterns 
-      + pattern.relPath.replace(/\\/g, '-').replace('.mustache', '/') 
-      + 'custom-' 
-      + pattern.relPath.replace(/\\/g, '-') + '.mustache', patternFile)
+    snippetTemplate.generateOutput(patternlab, pattern);
   }
 }
 
-function updateVersionDependentPatterns(patternlab, pattern, newVersion)  {
+function updateVersionDependentPatterns(dfsdfsdf, pattern, newVersion)  {
   for (var i = 0; i < pattern.lineageR.length; i++) {
     var currentPattern = pattern.lineageR[i];
     var lineagePathParts = currentPattern.lineagePath.split('\\');
     var lastPartParts = lineagePathParts[lineagePathParts.length - 1].split('.');
-    var actualPattern = getPatternByName(patternlab, lastPartParts[0]);
+    var actualPattern = util.getPatternByName(patternlab, lastPartParts[0]);
 
     if (actualPattern) {
-      var currentPatternMarkdown = getPatternMarkdownObject(patternlab, actualPattern);
+      var currentPatternMarkdown = util.getPatternMarkdownObject(patternlab, actualPattern);
       currentPatternMarkdown.data["version"] = newVersion;
-      setPatternMarkdownObject(patternlab, actualPattern, currentPatternMarkdown);
+      util.setPatternMarkdownObject(patternlab, actualPattern, currentPatternMarkdown);
 
       updateVersionDependentPatterns(patternlab, actualPattern, newVersion);
     } else {
       console.log('Could not find pattern with name: ' + lastPartParts[0]);
     }
   }
-}
-
-function getPatternMarkdownObject(patternlab, pattern) {
-    var markdownFile = ''
-    if (pattern.relPath.indexOf('.mustache') !== -1 
-      && fileExists(path.resolve(patternlab.config.paths.source.patterns 
-        + pattern.relPath.replace('.mustache', '.md')))) {
-        markdownFile = fs.readFileSync(path.resolve(patternlab.config.paths.source.patterns 
-        + pattern.relPath.replace('.mustache', '.md')), 'utf8');
-    }
-
-    return matter(markdownFile);
-}
-
-function setPatternMarkdownObject(patternlab, pattern, markdownObject) {
-  return fs.outputFileSync(path.resolve(patternlab.config.paths.source.patterns + pattern.relPath.replace('.mustache', '.md')), 
-                            matter.stringify(markdownObject.content.replace(/^[\r\n]+|[\r\n]+$/g, ""), markdownObject.data));
-}
-
-function getPatternByName(patternlab, patternName) {
-  for (var i = 0; i < patternlab.patterns.length; i++) {
-    if (patternlab.patterns[i].name === patternName) {
-      return patternlab.patterns[i];
-    }
-  }
-
-  return null;
 }
 
 function registerEvents (patternlab) {
